6a39722e3f502a8d0c2b2999655ad006b33d9851
Alessandro Bianchi <alex24br@yahoo.it>
2019-03-22 09:39:14 +0100

    ok

--- app/src/main/java/ch/supsi/dti/isin/meteoapp/fragments/ListFragment.java
+++ app/src/main/java/ch/supsi/dti/isin/meteoapp/fragments/ListFragment.java
@@ -5,7 +5,6 @@ import android.content.ContentValues;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
-import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.widget.LinearLayoutManager;
